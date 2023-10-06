eff34c3a421b167dd6484f32f2a34bf53a40b153
Jope2004 <85450379+Jope2004@users.noreply.github.com>
2023-10-06 07:36:52 -0500

    2
    Corregido

--- Parcial.java
+++ Parcial.java
@@ -1,50 +1,65 @@
-
 public class Parcial {
     static int numHotel() {
-        int hotel[] = {(int) (Math.random() * 7 + 1)};
-        return hotel[0];
+        int hotel = (int) (Math.random() * 7 + 1);
+        return hotel;
     }
+
     static int numHoras() {
-        int Hora[] = {(int) (Math.random() * 23 + 1)};
-        return Hora[0];
+        int hora = (int) (Math.random() * 24 + 1);
+        return hora;
     }
+
     static int numPisos() {
-        int Pisos[] = {(int) (Math.random() * 15 + 1)};
-        return Pisos[0];
+        int pisos = (int) (Math.random() * 15 + 1);
+        return pisos;
     }
-    static int Luz() {
-        int Luces = (int) (Math.random() * 3 + 1);
-        switch (Luces) {
+
+    static void Luz() {
+        int luces = (int) (Math.random() * 3 + 1);
+        switch (luces) {
             case 1:
-             double halogena = 0.25;
-             System.out.println("El tipo de luz es, halogena y suvalor es : "+ halogena);
-                
+                double halogena = 0.25;
+                System.out.println("El tipo de luz es halogena y su valor es: " + halogena);
                 break;
-            case 2: 
-            double led = 0.50;
-            System.out.println("El tipo de luz es, led y su valor es : "+ led);
+            case 2:
+                double led = 0.50;
+                System.out.println("El tipo de luz es led y su valor es: " + led);
                 break;
             case 3:
-            double multiled = 1;
-            System.out.println("El tipo de luz es, multiled y suvalor es : "+ multiled);
+                double multiled = 1.0;
+                System.out.println("El tipo de luz es multiled y su valor es: " + multiled);
+                break;
             default:
+                System.out.println("Tipo de luz desconocido");
                 break;
         }
-        return Luces;
     }
 
     public static void main(String[] args) {
-        int numeroHotel = numHotel();
-        System.out.println("Número de hotel: " + numeroHotel);
+        int numHoteles = 5; 
 
-        int numeroHoras = numHoras ();
-        System.out.println("Numero de horas: "+ numeroHoras);
+        for (int i = 1; i <= numHoteles; i++) {
+            System.out.println("Hotel " + i);
+            int numeroHotel = numHotel();
+            System.out.println("Número de hotel: " + numeroHotel);
 
-        int numerPisos = numPisos();
-        System.out.println("Numero de pisos "+ numerPisos); 
+            int numeroHoras = numHoras();
+            System.out.println("Número de horas: " + numeroHoras);
 
-        System.out.println( Luz() );
+            int numerPisos = numPisos();
+            System.out.println("Número de pisos: " + numerPisos);
+
+            Luz();
+
+            int cantidadLuz = (numerPisos * 3);
+            int LuzHotel = (cantidadLuz*numerPisos);
+
+            
 
 
+
+            
+
+        }
     }
 }
